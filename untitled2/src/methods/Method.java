669a8ffc074f1b42377b8460d420fd07ba90868e
Marinnna1 <58524607+Marinnna1@users.noreply.github.com>
2021-05-05 16:56:59 +0300

    Delete untitled2 directory

--- untitled2/src/methods/Method.java
+++
@@ -1,8 +0,0 @@
-package methods;
-
-public interface Method {
-
-    void doMethod(double a, double b, int n, int accuracy);
-
-    double getAccuracyOfNumericalIntegration(double a, double b, int n);
-}
